a2a1b22f8c7912216d6d0573a7208c0262dbb130
Adity00 <adityajagtap860@gmail.com>
2026-08-03 03:07:43 +0530

    Post and get learinig

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 @app.get("/")
@@ -29,3 +30,35 @@ def search(q):
     return{
         "query":q
     }
+
+@app.get("/calculator")
+def calculate(a:int,b:int):
+    return {
+        "a": a,
+        "b": b,
+        "sum":a + b
+    }
+
+class URLRequest(BaseModel):
+    url : str
+
+
+url_database = {}
+
+@app.post("/shorten")    
+def shorten(request:URLRequest):
+    short_code="abc123"
+    url_database[short_code]=request.url
+    return{
+        "originalUrl":request.url,
+        "shortcode":short_code
+    }
+
+@app.get("/{code}")
+def redirect(code:str):
+    if code in url_database:
+        return {"originalURL":url_database[code]
+        }
+    return{
+        "error":"Short code Not found in DB"
+    }    
