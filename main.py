57e8e5bec8c3c4e30d3739db3a91fd5c65b8ab90
Adity00 <adityajagtap860@gmail.com>
2026-08-06 02:51:04 +0530

    Logic connected with actual GET and POST and executed. Also learned to implement exception

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
+import random
+import string
 
 app = FastAPI()
 @app.get("/")
@@ -45,20 +47,34 @@ class URLRequest(BaseModel):
 
 url_database = {}
 
-@app.post("/shorten")    
-def shorten(request:URLRequest):
-    short_code="abc123"
-    url_database[short_code]=request.url
-    return{
-        "originalUrl":request.url,
-        "shortcode":short_code
+@app.post("/shorten")
+def shorten(request: URLRequest):
+    short_code = generate_short_code()
+    url_database[short_code] = request.url
+
+    print(url_database)
+
+    return {
+        "originalUrl": request.url,
+        "shortcode": short_code
     }
 
-@app.get("/{code}")
+@app.get("/code/{code}")
 def redirect(code:str):
     if code in url_database:
         return {"originalURL":url_database[code]
         }
-    return{
-        "error":"Short code Not found in DB"
-    }    
+    raise HTTPException(
+    status_code=404,
+    detail="Short code not found"
+)
+
+def generate_short_code(length=6):
+    res = ""
+    chars = string.digits+string.ascii_letters
+    while True:
+        res = ""
+        for i in range(length):
+            res+=random.choice(chars)
+        if res not in url_database:
+           return res
