7129c2ba69cd8b0f79579e8d32aecbf4659b3322
Adity00 <adityajagtap860@gmail.com>
2026-09-06 13:48:33 +0530

    race condition handling with UniqueViolation which check enter except block and coninues generating new code(create_short_url)

--- services/url_service.py
+++ services/url_service.py
@@ -1,6 +1,7 @@
 import random
 import string
 from datetime import datetime,timedelta
+from psycopg.errors import UniqueViolation
 
 from database.queries import create_url, get_url_for_redirect_db,increment_clicks
 
@@ -9,19 +10,25 @@ def generate_short_code(length=6):
     return ''.join(random.choice(characters) for _ in range(length))    
 
 def create_short_url(url, expire_in=None):
-    short_code = generate_short_code()
 
     expires_at=None
 
     if expire_in is not None:
         expires_at = datetime.now() + timedelta(seconds=expire_in)
 
-    create_url(
-        short_code=short_code,
-        url=str(url),
-        expires_at=expires_at
-    )
-    return short_code,expires_at
+    while True:
+        short_code = generate_short_code()
+
+        try:
+            create_url(
+                short_code=short_code,
+                url=str(url),
+                expires_at=expires_at
+            )
+            return short_code,expires_at
+        
+        except UniqueViolation:
+            continue
 
 def get_url_for_redirect(code):
     record = get_url_for_redirect_db(code)
