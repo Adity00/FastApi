314e4ff5ad69cf46ea4a4e7938aa68d11068ac06
Adity00 <adityajagtap860@gmail.com>
2026-09-10 01:26:26 +0530

    env config validation

--- database/connection.py
+++ database/connection.py
@@ -1,18 +1,36 @@
 import os
+
 from psycopg_pool import ConnectionPool
 from dotenv import load_dotenv
 
 load_dotenv()
 
+db_host = os.getenv("DB_HOST")
+db_port = os.getenv("DB_PORT")
+db_name = os.getenv("DB_NAME")
+db_user = os.getenv("DB_USER")
+db_password = os.getenv("DB_PASSWORD")
+
+
+if any([
+    not db_host,
+    not db_port,
+    not db_name,
+    not db_user,
+    not db_password
+]):
+    raise ValueError("Missing required database configuration")
+
+
 pool = ConnectionPool(
-    conninfo = (
-        f'host={os.getenv("DB_HOST")} ' 
-        f'port={os.getenv('DB_PORT')} '
-        f'dbname={os.getenv("DB_NAME")} '
-        f'user={os.getenv("DB_USER")} '
-        f'password={os.getenv("DB_PASSWORD")} '
+    conninfo=(
+        f"host={db_host} "
+        f"port={db_port} "
+        f"dbname={db_name} "
+        f"user={db_user} "
+        f"password={db_password} "
     ),
     min_size=1,
     max_size=5,
-    open = True
+    open=True
 )
